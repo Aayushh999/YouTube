dc8d76eeded5e7a141a7ed256bc73166004cff08
Ayush Sharma <ayush.sharmaod21@gmail.com>
2024-09-14 01:16:44 +0530

    redux and router structure setup

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<App />}>
+      
+    </Route>
+  )
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store = {store}>
+      <RouterProvider router = {router}/>
+    </Provider>
   </StrictMode>,
 )
